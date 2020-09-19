5d910f1ae0f3a3115b7fb8479053923898c71ba8
Hamza ben romdhane <hamzabnromdhane@gmail.com>
2020-09-19 16:54:58 +0100

    must work plse

--- build.sh
+++ build.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
-# set -xe
+set -xe
 
 rm -rf ./app
 mkdir ./app
-cp -r ./.ebextensions ./app
 cp -r ./public ./app
 cp -r ./tests ./app
-
 cp -r ./app.js ./app
-cp -r ./buildspec.yml ./app
 cp -r ./package.json ./app
-cp -r ./template-configuration.json ./app
-cp -r ./template.yml ./app
 cd app
+cp -r ./* ../
+cd ..
+rm -rf ./app
 # cd ./app
 # zip -rm app.zip .
+
+# aws config files/folders
+# cp -r ./.ebextensions ./app
+# cp -r ./buildspec.yml ./app
+# cp -r ./template-configuration.json ./app
+# cp -r ./template.yml ./app
